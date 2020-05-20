72a7594f066d9d1cba272683e09cb860c210a1c8
juan-ivan-NV <coldcram14@hotmail.com>
2020-05-20 18:32:10 -0500

    send emails ready

--- main_captures.py
+++ main_captures.py
@@ -1,6 +1,10 @@
 import recognizer_things_cvlib
 import recognizer_fruits_cnn
 import recognizer_face_ibm
+import send_mail
+
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import cv2
@@ -73,6 +77,37 @@ total_items.loc['Total'] = pd.Series(total_items['Price'].sum(), index = ['Price
 print(buyer_identity)
 print('\n\n')
 print(total_items)
+
+
+products = dict(fruits_class.items() | things_class.items())
+
+total_items = pd.DataFrame(products.items(),columns=['Product', 'Price'])
+total_items.loc['Total'] = pd.Series(total_items['Price'].sum(), index = ['Price'])
+total_items=total_items.fillna(total_items.index[-2]+1)
+total_items['Items'] = total_items.index
+total_items = total_items[ ['Items'] + [ col for col in total_items.columns if col != 'Items' ] ]
+
+fig, ax =plt.subplots(figsize=(12,4))
+ax.axis('tight')
+ax.axis('off')
+the_table = ax.table(cellText=total_items.values,colLabels=total_items.columns,loc='center')
+
+pp = PdfPages("tickets.pdf")
+pp.savefig(fig, bbox_inches='tight')
+pp.close()
+
+print(send_mail.sending_mail('tickets.pdf'))
+#exec(open('file.py').read())
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ send_mail.py
@@ -0,0 +1,46 @@
+import smtplib, ssl
+import socket
+import getpass
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.base import MIMEBase
+from email import encoders
+
+#def sending_mail(filepdf):
+
+email_user = "juanivannb142@gmail.com"
+email_send = "coldcram14@hotmail.com"
+subject = 'JINB'
+    #password = getpass.getpass("Type your password and press enter: ")
+    #password = 'obgekeoucjsoborx'
+
+msg = MIMEMultipart()
+msg['From'] = email_user
+msg['To'] = email_send
+msg['Subject'] = subject
+
+body = 'Hi mister this is your ticket!'
+msg.attach(MIMEText(body,'plain'))
+
+filename = 'ticket.txt'
+#filename = filepdf
+attachment = open(filename,'rb')
+
+part = MIMEBase('application','octet-stream')
+part.set_payload((attachment).read())
+encoders.encode_base64(part)
+part.add_header('Content-Disposition','attachment: filename = ' + filename)
+
+msg.attach(part)
+text = msg.as_string()
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()
+server.login(email_user, 'obgekeoucjsoborx')
+
+server.sendmail(email_user,email_send,text)
+server.quit()
+
+    #return 'mail ok'
+
+#filepdf = 'ticket.txt'
+#print(sending_mail(filepdf))
